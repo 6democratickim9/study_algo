b3fd5760de6f3c8359c21613a7575fedffd21634
Bittah Swami <41178536+6democratickim9@users.noreply.github.com>
2021-07-16 00:05:21 +0900

    0716

--- programmers/descending.py
+++ programmers/descending.py
@@ -2,7 +2,8 @@ def solution(n):
     answer=[]
     for i in range(len(str(n))):answer.append(int(str(n)[i]))
     answer.sort(reverse=True)
-    return str(answer)
+    return int("".join([str(_) for _ in answer]))
+    # join 이용해서 ""달린 내용들 빼내기
 print(solution(118372))
 # print(str(i) for i in str(118372))
 # for i in str(118363):print (i)
